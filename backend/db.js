8390672b5a8a08b21a977f110bfc59ecd7e14314
M-ABHIRAM36 <abhicoding36@gmail.com>
2026-03-10 00:15:54 +0530

    just changed name of db

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/resume_screener';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/resume_screening_system';
 
 async function connectDB() {
   try {
