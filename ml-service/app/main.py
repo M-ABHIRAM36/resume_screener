200f619ae917773519cf5eb2121c901216a9870d
M.Abhiram <abhicoding36@gmail.com>
2026-03-29 01:28:10 +0530

    Implement home route for ML service status for render deployment cheking
    Added a home route to return service status.

--- ml-service/app/main.py
+++ ml-service/app/main.py
@@ -1,9 +1,17 @@
+# from fastapi import FastAPI
+# from .routes import router as routes
+# import uvicorn
+
+# app = FastAPI(title='Resume ML Service')
+# app.include_router(routes)
+
+# if __name__ == '__main__':
+#     uvicorn.run('app.main:app', host='0.0.0.0', port=8000, reload=False)
+
 from fastapi import FastAPI
-from .routes import router as routes
-import uvicorn
 
 app = FastAPI(title='Resume ML Service')
-app.include_router(routes)
 
-if __name__ == '__main__':
-    uvicorn.run('app.main:app', host='0.0.0.0', port=8000, reload=False)
+@app.get("/")
+def home():
+    return {"status": "ML service running"}
